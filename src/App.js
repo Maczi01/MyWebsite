825e455993c5643b28cb361068097498622492fd
matchi01 <mateusz.w.twardy@gmail.com>
2021-05-14 14:55:49 +0200

    Added pages (Home, projects, resume)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import AboutContent from "./About/AboutContent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <AboutContent/>
+                    </Route>
+                    <Route exact path='/projects'>
+                        <AboutContent/>
+                    </Route>
+                    <Route exact path='/resume'>
+                        <ResumeContent/>
+                    </Route>
+                    <Route exact path='/'>
+                        <AboutContent/>
+                    </Route>
+                </Switch>
+            </Router>
+        </>
+    );
 }
 
 export default App;
