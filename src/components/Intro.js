f2a353d3486fca390f230b11d0bc58f3bc397143
matchi01 <mateusz.w.twardy@gmail.com>
2021-05-26 22:24:12 +0200

    Changes in main container

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,5 +1,6 @@
 import Typical from "react-typical";
 import styled from "styled-components";
+import Typewriter from 'typewriter-effect/dist/core';
 
 const IntroWrapper = styled.div`
   margin: 100px;
@@ -9,6 +10,18 @@ const IntroWrapper = styled.div`
   width: 600px;
   height: 220px;
   font-weight: bold;
+  
+    color: ${({theme})=> theme.colors.yellow};
+    
+  text-shadow:
+    0 0 5px ${({theme})=> theme.colors.gray},
+    0 0 10px currentColor,
+    0 0 20px currentColor,
+    0 0 40px currentColor,
+    0 0 80px currentColor,
+    0 0 90px currentColor,
+    0 0 100px currentColor,
+    0 0 110px currentColor;
 `
 
 const Intro = () => (
@@ -16,7 +29,7 @@ const Intro = () => (
         <Typical
             steps={['Cześć! 👋, jestem Mati.\n' +
             '                    Jestem początkującym Fullstack Developerem.', 1000]}
-            loop={Infinity}
+            loop={1}
             wrapper="p"
         />
     </IntroWrapper>
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-     width: 80vw;        
+     width: 80vw; 
+     height:100vh;       
      display: flex;
      flex-direction: column;
      margin: 0 auto;
